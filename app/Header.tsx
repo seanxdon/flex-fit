05578f76e2dcbb3432ccd9fee63b0b754e6f68a8
Sean McDaniel <seanhb714@gmail.com>
2023-10-21 14:40:17 -0700

    Navigation  - Workout, Exercises, My Plan

--- app/Header.tsx
+++ app/Header.tsx
@@ -2,9 +2,9 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <header className="flex justify-between items-center w-full border-b-2 sm:pb-6 ">
+    <header className="flex justify-between items-center w-full border-b-2 pb-6 ">
       <Link href="/" className="flex">
-        <h1 className="sm:text-4xl tracking-tight">
+        <h1 className="text-4xl tracking-tight">
           flex<b>fit</b>
         </h1>
       </Link>
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Image from 'next/image'
 import Header from './Header'
+import Navigation from './Navigation'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex flex-col items-center justify-between px-10 py-20">
       <Header />
+      <Navigation />
     </main>
   )
 }

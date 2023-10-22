f1894e72de57ffcf071b3a2f356c9b2781054e2c
Sean McDaniel <seanhb714@gmail.com>
2023-10-22 01:19:21 -0700

    AI response returned and formatted by day of the week

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,26 +1,31 @@
-import OpenAI from 'openai';
-import { OpenAIStream, StreamingTextResponse } from 'ai';
+import OpenAI from 'openai'
+import { OpenAIStream, StreamingTextResponse } from 'ai'
  
 // Create an OpenAI API client (that's edge friendly!)
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
-});
+  apiKey: process.env.OPENAI_API_KEY
+})
  
 // IMPORTANT! Set the runtime to edge
-export const runtime = 'edge';
+export const runtime = 'edge'
  
 export async function POST(req: Request) {
   // Extract the `messages` from the body of the request
-  const { messages } = await req.json();
+  const { workout, days, exercises } = await req.json()
  
   // Ask OpenAI for a streaming chat completion given the prompt
   const response = await openai.chat.completions.create({
     model: 'gpt-3.5-turbo',
     stream: true,
-    messages,
-  });
+    messages : [
+      {
+        role: "user",
+        content: `You are a certified personal trainer with over 10 years of experience. Please create a workout plan for me to follow for the each day of the week. I am working out ${days} days out of the week with no exceptions to the plan. I want to follow a ${workout} routine. Each day I am working out I am following one of these areas. Please provide the day of the week for the first portion of the response and exercises with the amount of sets and rep recommended. I want to have ${exercises} exercises per day with no exceptions to the plan. No need for an explanation and return in the same format every time.`
+      }
+    ]
+  })
   // Convert the response into a friendly text-stream
-  const stream = OpenAIStream(response);
+  const stream = OpenAIStream(response)
   // Respond with the stream
-  return new StreamingTextResponse(stream);
+  return new StreamingTextResponse(stream)
 }
--- app/push-pull-legs/page.tsx
+++ app/push-pull-legs/page.tsx
@@ -1,35 +1,96 @@
 'use client'
-import { useChat } from 'ai/react';
-
+ 
+import { useChat } from 'ai/react'
+import Link from 'next/link';
+import { useState } from 'react';
+ 
 export default function PushPullLegs() {
+  const [days, setDays] = useState("");
+  const [exercises, setExercises] = useState("");
 
-  const { messages, input, handleInputChange, handleSubmit } = useChat();
+  const { messages , handleInputChange, handleSubmit } = 
+  useChat({
+    body: {
+      workout: "Push Pull Legs",
+      days: days,
+      exercises: exercises,
+    }
+  })
 
+  const onChangeDays = (e: any) => {
+    setDays(e.target.value);
+    handleInputChange(e)
+  }
+
+  const onChangeExercises = (e: any) => {
+    setExercises(e.target.value);
+    handleInputChange(e)
+  }
+  
+  const onSubmit = (e: any) => {
+    handleSubmit(e);
+  };
+
+  const lastMessage = messages[messages.length - 1];
+  const generatedWorkout = lastMessage?.role === "assistant" ? lastMessage.content : null;
 
   return (
-    <main className="mx-auto w-full h-screen p-24 flex flex-col">
-      <section className="mb-auto m">
-        {messages.map(m => (
-          <div className="mb-4" key={m.id}>
-            {m.role === 'user' ? 'User: ' : 'AI: '}
-            {m.content}
-          </div>
-        ))}
-      </section>
-      <form className="flex space-x-4" onSubmit={handleSubmit}>
-        <input
-          className="rounded-md p-2 text-black"
-          value={input}
-          onChange={handleInputChange}
-          placeholder="Say something..."
-        />
-        <button
-          className="border-solid border-2 border-white p-2 rounded-md"
-          type="submit"
-        >
-          Send
-        </button>
-      </form>
-    </main>
-  );
+    <>
+    <div className="bg-repeat bg-gray-800 bg-[url('https://images.unsplash.com/photo-1652363722833-509b3aac287b?auto=format&fit=crop&q=80&w=2940&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] bg-black/70 ">
+      <main className="mx-auto w-full h-screen max-w-lg flex flex-col p-2">
+        <form className="my-10 flex flex-row" onSubmit={onSubmit}>
+          <input
+            className="rounded-md p-2 m-2 text-black"
+            type="number"
+            max={7}
+            onChange={onChangeDays}
+            placeholder="# of workouts"
+          />
+          <input
+            className="rounded-md p-2 m-2 text-black"
+            type="number"
+            max={20}
+            onChange={onChangeExercises}
+            placeholder="# of exercises"
+          />
+          <button
+            className="border-none bg-blue-400 hover:bg-blue-500 p-2 m-2 rounded-md w-full"
+            type="submit"
+          >
+            Generate
+          </button>
+          <Link href={"/"}>
+          <button
+            className="border-none bg-blue-400 hover:bg-blue-500 p-2 m-2 rounded-md w-full"
+            type="submit"
+          >
+            Home
+          </button>
+          </Link>
+        </form>
+        <section className="mb-auto">
+        {generatedWorkout && (
+          <>
+            <div className="space-y-8 w-full flex flex-col items-center justify-center max-w-xl mx-auto">
+              {generatedWorkout
+                .substring(generatedWorkout.indexOf('Monday') + 0)
+                .split(/[\r]?\n[\r]?\n/g)
+                .map((workouts) => {
+                  return (
+                    <div
+                      className="bg-white text-black rounded-xl shadow-md p-4 hover:bg-gray-100 transition cursor-copy border"
+                      key={workouts}
+                    >
+                      <p className="text-xs">{workouts}</p>
+                    </div>
+                  );
+                })}
+            </div>
+          </>
+          )}
+        </section>
+      </main>
+    </div>
+    </>
+  )
 }

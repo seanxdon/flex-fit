17c86347b74e25718829faf710c704898bcec8ae
Sean McDaniel <seanhb714@gmail.com>
2023-10-22 13:03:21 -0700

    Mobile Styling - Workout form and response

--- app/push-pull-legs/page.tsx
+++ app/push-pull-legs/page.tsx
@@ -37,45 +37,49 @@ export default function PushPullLegs() {
 
   return (
     <>
-    <div className="bg-repeat	 bg-black/70 bg-[url('https://images.unsplash.com/photo-1652363722833-509b3aac287b?auto=format&fit=crop&q=80&w=2940&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')]">
+    <div className="bg-repeat min-h-max bg-black/70 bg-[url('https://images.unsplash.com/photo-1652363722833-509b3aac287b?auto=format&fit=crop&q=80&w=2940&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')]">
     <Header />
-      <main className="mx-auto w-full h-screen flex flex-col p-2">
-        <form className="my-10 flex flex-row" onSubmit={onSubmit}>
-          <input
-            className="rounded-md p-2 m-2 text-black w-1/3"
-            type="number"
-            min={3}
-            max={7}
-            onChange={onChangeDays}
-            placeholder="# of workouts per week"
-          />
-          <input
-            className="rounded-md p-2 m-2 text-black w-1/3"
-            type="number"
-            min={4}
-            max={20}
-            onChange={onChangeExercises}
-            placeholder="# of exercises per workout"
-          />
-          <button
-            className="border-none bg-cyan-500 hover:bg-cyan-400 p-2 m-2 rounded-md "
-            type="submit"
-          >
-            Generate Workout
-          </button>
-          <Link href={"/"}>
-          <button
-            className="border-none bg-cyan-500 hover:bg-cyan-400 p-2 m-2 rounded-md"
-            type="submit"
-          >
-            Home
-          </button>
-          </Link>
+      <main>
+        <form className="my-10 flex flex-row max-[800px]:flex-col justify-center text-center" onSubmit={onSubmit}>
+          <div>
+            <input
+              className="rounded-md p-2 m-2 text-black w-60"
+              type="number"
+              min={3}
+              max={7}
+              onChange={onChangeDays}
+              placeholder="# of workouts per week"
+            />
+            <input
+              className="rounded-md p-2 m-2 text-black w-60"
+              type="number"
+              min={4}
+              max={20}
+              onChange={onChangeExercises}
+              placeholder="# of exercises per workout"
+            />
+          </div>
+          <div>
+            <button
+              className="border-none bg-cyan-500 hover:bg-cyan-400 p-2 m-2 rounded-md w-30"
+              type="submit"
+            >
+              Generate Workout
+            </button>
+            <Link href={"/"}>
+              <button
+                className="border-none bg-cyan-500 hover:bg-cyan-400 p-2 m-2 rounded-md w-16"
+                type="submit"
+              >
+                Home
+              </button>
+            </Link>
+          </div>
         </form>
         <section className="mb-auto">
         {generatedWorkout && (
           <>
-            <div className="space-y-2 w-full flex flex-col items-center justify-center max-w-xl mx-auto">
+            <div className="space-y-2 px-5 w-full flex flex-col items-center justify-center max-w-xl mx-auto">
               {generatedWorkout
                 .substring(generatedWorkout.indexOf('Monday') + 0)
                 .split(/[\r]?\n[\r]?\n/g)
@@ -85,7 +89,7 @@ export default function PushPullLegs() {
                       className="bg-white text-black rounded-xl shadow-md p-4 hover:bg-gray-100 transition border"
                       key={workouts}
                     >
-                      <p className="text-sm">{workouts}</p>
+                      <p className="">{workouts}</p>
                     </div>
                   );
                 })}

7eea08b70d604671b209a0b6fbab9b2efc61eaa5
Sean McDaniel <seanhb714@gmail.com>
2023-10-24 18:04:51 -0700

    button removed on my Plan

--- app/my-plan/page.tsx
+++ app/my-plan/page.tsx
@@ -22,12 +22,6 @@ export default function MyPlan() {
       <div className="px-[9%] flex flex-col justify-center">
         <h1 className="py-16 uppercase font-sans tracking-tight text-5xl text-cyan-200	font-bold">My <br></br>Workout</h1>
         <textarea rows={10} className="p-2.5 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Your workout routine for the week:" value={workoutPlan} onChange={onChangeWorkoutPlan}></textarea>
-        <button
-              className="border-none bg-cyan-500 hover:bg-cyan-400 p-2 m-2 rounded-md w-60"
-              type="submit"
-            >
-              Generate Workout
-            </button>
       </div>
     </div>
   )
